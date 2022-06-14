46972cb4142517ab959e12886f57e329623a49db
Divya Murali <divya7747@gmail.com>
2022-06-14 14:56:38 +0530

    MerchantDetails included in PaymentRequest

--- src/main/java/xyz/mowrish/springpaymentqr/controller/PaymentsController.java
+++ src/main/java/xyz/mowrish/springpaymentqr/controller/PaymentsController.java
@@ -8,13 +8,19 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import xyz.mowrish.springpaymentqr.model.PaymentRequest;
 
 import java.net.InetAddress;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController("/api")
@@ -26,11 +32,21 @@ public class PaymentsController {
     @SneakyThrows
     @PostMapping("/createPaymentQr")
     public ResponseEntity<String> createPaymentQr(@RequestBody PaymentRequest paymentRequest) {
-        String uuid = UUID.randomUUID().toString();
-        String url = "http://" + InetAddress.getLocalHost().getHostAddress() + ":" + serverPort + "/makePayment/" + uuid;
+        String txId = UUID.randomUUID().toString();
+
+        MultiValueMap<String, String> urlParams = new LinkedMultiValueMap<>();
+        urlParams.add("transactionId", txId);
+        urlParams.add("merchantName", paymentRequest.getMerchantName());
+        urlParams.add("merchantUpi", paymentRequest.getMerchantUpiId());
+        urlParams.add("amount", String.valueOf(paymentRequest.getAmount()));
+
+        String url = UriComponentsBuilder.newInstance()
+                .scheme("http").host(InetAddress.getLocalHost().getHostAddress())
+                .path("/makePayment/").queryParams(urlParams).build().toUriString();
+
         QRCodeWriter barcodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = barcodeWriter.encode(url, BarcodeFormat.QR_CODE, 200, 200);
-        Path path = FileSystems.getDefault().getPath(uuid + ".png");
+        Path path = FileSystems.getDefault().getPath(txId + ".png");
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
         return new ResponseEntity<>(url, HttpStatus.OK);
     }
